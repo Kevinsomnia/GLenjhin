0f9dcb813757db2402ddee18091f9a520d67533e
Kevin Tong <kevinsomnia17@gmail.com>
2021-09-26 17:04:35 -0400

    Camera and scene component stub

---
+++ src/Camera.h
@@ -0,0 +1,11 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void render();
+
+#endif // CAMERA_H
---
+++ src/Scene.h
@@ -0,0 +1,11 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void render();
+
+#endif // SCENE_H
