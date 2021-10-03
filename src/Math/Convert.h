e80d18a47ef0348a3c4d5dc26cec2f937dd19979
Kevin Tong <kevinsomnia17@gmail.com>
2021-10-03 16:37:06 -0400

    Inline micro-optimization for unit conversion functions

--- src/Math/Convert.h
+++ src/Math/Convert.h
@@ -4,8 +4,22 @@
 #include <time.h>
 #include "Math.h"
 
-double clockTicksToSeconds(clock_t clk);
-float degToRad(float degrees);
-float radToDeg(float radians);
+#define DEG_TO_RAD PI_FLOAT / 180.0f
+#define RAD_TO_DEG 180.0f / PI_FLOAT
+
+inline double clockTicksToSeconds(clock_t clk)
+{
+	return clk / (double)CLOCKS_PER_SEC;
+}
+
+inline float degToRad(float degrees)
+{
+	return degrees * DEG_TO_RAD;
+}
+
+inline float radToDeg(float radians)
+{
+	return radians * RAD_TO_DEG;
+}
 
 #endif // CONVERT_H
