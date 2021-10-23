0d831c48748693576287940e94da07908ade7e55
Kevin Tong <kevinsomnia17@gmail.com>
2021-10-23 05:03:41 -0400

    Added Dear ImGui 1.85

--- src/Core/GameContainer.cpp
+++ src/Core/GameContainer.cpp
@@ -7,9 +7,15 @@ GameContainer::GameContainer(GLFWwindow* window) : m_MainWindow(window), m_Curre
     // Enable back face culling
     glEnable(GL_CULL_FACE);
 
+    // Core
     Time::Init();
     Input::Init(window);
     MeshPrimitives::Init();
+
+    // ImGui
+    ImGui::CreateContext();
+    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
 
     // Load scene.
     m_CurrentScene = new Scene();
@@ -30,9 +36,22 @@ void GameContainer::update(double deltaTime)
 
 void GameContainer::render()
 {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.5f, 0.7f, 0.825f, 1.0f);
 
     if (m_CurrentScene)
         m_CurrentScene->draw();
+
+    onGUI();
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+
+void GameContainer::onGUI()
+{
 }
--- src/Core/GameContainer.h
+++ src/Core/GameContainer.h
@@ -1,11 +1,17 @@
 #ifndef GAMECONTAINER_H
 #define GAMECONTAINER_H
 
+#define IMGUI_IMPL_OPENGL_ES3
+
 #include <iostream>
 #include "../Interaction/Input.h"
 #include "../Renderer/MeshPrimitives.h"
 #include "Scene.h"
 #include "Time.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 using std::cout;
 using std::endl;
@@ -17,6 +23,7 @@ public:
     ~GameContainer();
     void update(double deltaTime);
     void render();
+    void onGUI();
 private:
     GLFWwindow* m_MainWindow;
     Scene* m_CurrentScene;
