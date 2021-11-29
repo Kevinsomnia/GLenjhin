0c0ee90b5613fb2529352edebfdb59bd105df571
Kevin Tong <kevinsomnia17@gmail.com>
2021-11-29 03:59:27 -0500

    Remove null check from MeshRenderer draw function
    If you don't assign a mesh and issue a draw call, then that's your fault. If we later want a mesh to be hidden, then that logic should be handled from the Entity class

--- src/Renderer/MeshRenderer.cpp
+++ src/Renderer/MeshRenderer.cpp
@@ -10,21 +10,18 @@ MeshRenderer::~MeshRenderer()
 
 void MeshRenderer::draw(const Vector3& cameraPos, const Matrix4x4& vp, const Matrix4x4& model, const std::vector<Light*>& lights)
 {
-    if (m_Mesh)
+    if (m_Material)
     {
-        if (m_Material)
-        {
-            m_Material->setMatrix("u_VP", vp);
-            m_Material->setMatrix("u_Model", model);
-            m_Material->setVector("u_CameraPos", cameraPos);
-            m_Material->bind();
-        }
-
-        // NOTE: multiple lights of same type are not supported!
-        for (Light* light : lights)
-            light->bind(m_Material);
-
-        m_Mesh->bind();
-        m_Mesh->draw();
+        m_Material->setMatrix("u_VP", vp);
+        m_Material->setMatrix("u_Model", model);
+        m_Material->setVector("u_CameraPos", cameraPos);
+        m_Material->bind();
     }
+
+    // NOTE: multiple lights of same type are not supported!
+    for (Light* light : lights)
+        light->bind(m_Material);
+
+    m_Mesh->bind();
+    m_Mesh->draw();
 }
